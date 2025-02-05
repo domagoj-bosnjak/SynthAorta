a78d62f359e605817a59476c25f81203fffc48b3
Domagoj Bosnjak <bosnjak@tugraz.at>
2025-02-05 12:10:50 +0100

    Initial commit.

---
+++ Code/SynthAortaLoadLinMesh.m
@@ -0,0 +1,80 @@
+function Mesh = SynthAortaLoadLinMesh(Index, RefLevel)
+
+%% Input function for loading a mesh
+
+%% All possible calls:
+% Mesh = SynthAortaLoadMesh(1, 8);
+% Mesh = SynthAortaLoadMesh(1, 4);
+% Mesh = SynthAortaLoadMesh(1, 2);
+% Mesh = SynthAortaLoadMesh(1, 1);
+
+%% Filenames and basic verification
+%% the paths assume SynthAortaDataset is a first-level subfolder
+FilenameXYZ = "./SynthAortaDataset/MeshNodes/MeshNodes_" + num2str(Index) + ".bin";
+FilenameConnMat = ...
+    "./SynthAortaDataset/ConnectivityMatrices/ConnMat_Ord_1_Ref_" + ...
+    num2str(RefLevel) + ".bin";
+FilenameConnMatBdry = ...
+    "./SynthAortaDataset/ConnectivityMatrices/ConnMatBdry_Ord_1_Ref_" + ...
+    num2str(RefLevel) + ".bin";
+FilenameGlobalInds = ...
+    "./SynthAortaDataset/ConnectivityMatrices/GlobalInds_Ord_1_Ref_" + ...
+    num2str(RefLevel) + ".bin";
+% Check that files exist
+if ~isfile(FilenameXYZ) || ~isfile(FilenameConnMat)
+    error("File does not exist; likely invalid RefLevel!" + ...
+        " Check SynthAortaLoadLinMesh for details");
+end
+% GlobalInds is not needed for the most refined mesh
+if ~isfile(FilenameGlobalInds) && RefLevel ~= 8
+    error("File does not exist; likely invalid RefLevel!" + ...
+        " Check SynthAortaLoadLinMesh for details");
+end
+
+%% Load the nodes and connectivity
+% General formula needed for reshaping
+NodeNumPerElem = 8;
+NodeNumPerElemSurf = 4;
+
+% Load xyz nodes
+fileID = fopen(FilenameXYZ, "r");
+MeshNodes = fread(fileID, "single");
+fclose(fileID);
+% Reshape from a vector to xyz; 3 because there are 3 coordinates: x,y,z
+MeshNodes = reshape(MeshNodes, [3, length(MeshNodes)/3]);
+MeshNodes = MeshNodes';
+
+% If the desired mesh is coarser, indices need to be selected out
+if RefLevel < 8
+    fileID = fopen(FilenameGlobalInds, "r");
+    GlobalInds = fread(fileID, "uint32");
+    fclose(fileID);
+
+    % Select only those mesh nodes
+    MeshNodes = MeshNodes(GlobalInds, :);
+end
+
+% Load connectivity matrix
+fileID = fopen(FilenameConnMat, "r");
+ConnMat = fread(fileID, "uint32");
+ConnMat = reshape(ConnMat, [NodeNumPerElem, length(ConnMat)/NodeNumPerElem]);
+ConnMat = ConnMat';
+fclose(fileID);
+
+% Load connectivity matrix of the boundary
+fileID = fopen(FilenameConnMatBdry, "r");
+ConnMatBdry = fread(fileID, "uint32");
+ConnMatBdry = reshape(ConnMatBdry, [NodeNumPerElemSurf, length(ConnMatBdry)/NodeNumPerElemSurf]);
+ConnMatBdry = ConnMatBdry';
+fclose(fileID);
+
+%% Set up the mesh structure
+Mesh.xx = MeshNodes(:, 1);
+Mesh.yy = MeshNodes(:, 2);
+Mesh.zz = MeshNodes(:, 3);
+Mesh.NodeNum = length(Mesh.xx);
+
+Mesh.ConnMat = ConnMat;
+Mesh.ConnMatBdry = ConnMatBdry;
+Mesh.ElemNum = size(ConnMat, 1);
+Mesh.NodeNumPerElem = NodeNumPerElem;
---
+++ Code/SynthAortaLoadSkeleton.m
@@ -0,0 +1,92 @@
+function Skeleton = SynthAortaLoadSkeleton(Index)
+
+%% Load and visualize a skeleton with radius information at the 
+%% given index
+
+%% Example call
+% SynthAortaLoadSkeleton(1);
+
+PlotFlag = 1; % do you want to plot? 
+
+%% Filenames and basic verification
+%% the paths assume SynthAortaDataset is a first-level subfolder
+SkeletonXYZInputFilename = ...
+    "./SynthAortaDataset/SkeletonData/SkeletonNodes_" + ...
+    num2str(Index) + ".bin";
+FilenameSkeletonConnMat = ...
+    "./SynthAortaDataset/ConnectivityMatrices/" + ...
+    "Skeleton_ConnMat.bin";
+SkeletonRadiiInputFilename = ...
+    "./SynthAortaDataset/SkeletonData/SkeletonRadii_" + ...
+    num2str(Index) + ".bin";
+%% Skeleton loading
+fileID = fopen(SkeletonXYZInputFilename, 'r');
+SkeletonXYZ = fread(fileID, 'single');
+fclose(fileID);
+
+% Reshape
+SkeletonXYZ = reshape(SkeletonXYZ, [3, length(SkeletonXYZ)/3]);
+SkeletonXYZ = SkeletonXYZ';
+
+NodeNumPerElem = 2;
+fileID = fopen(FilenameSkeletonConnMat, 'r');
+ConnMat = fread(fileID, 'uint32');
+ConnMat = reshape(ConnMat, [NodeNumPerElem, length(ConnMat)/NodeNumPerElem]);
+ConnMat = ConnMat';
+fclose(fileID);
+
+% Radii loading
+fileID = fopen(SkeletonRadiiInputFilename,'r');
+RadVec = fread(fileID, 'single');
+fclose(fileID);
+
+%% Assemble the skeleton
+Skeleton.xx = SkeletonXYZ(:,1);
+Skeleton.yy = SkeletonXYZ(:,2);
+Skeleton.zz = SkeletonXYZ(:,3);
+Skeleton.NodeNumPerElem = 2;
+Skeleton.NodeNum = length(Skeleton.xx);
+Skeleton.ElemNum = size(ConnMat, 1);
+Skeleton.ConnMat = ConnMat;
+
+%% Plot the skeleton
+if PlotFlag 
+    fprintf("Plotting skeleton!\n")
+    figure
+    view(3)
+    hold on
+    for i = 1 : Skeleton.ElemNum
+        xx = Skeleton.xx(Skeleton.ConnMat(i,:));
+        yy = Skeleton.yy(Skeleton.ConnMat(i,:));
+        zz = Skeleton.zz(Skeleton.ConnMat(i,:));
+
+        plot3(xx, yy, zz, 'k-', 'Marker', 'o', 'MarkerSize', 10,...
+            'MarkerFaceColor', 'm', 'MarkerEdgeColor', 'm', 'LineWidth', 5);
+        hold on
+    end
+    axis equal
+    hold off
+    figure
+    view(3)
+    hold on
+    for i = 1 : Skeleton.ElemNum
+        xx = Skeleton.xx(Skeleton.ConnMat(i,:));
+        yy = Skeleton.yy(Skeleton.ConnMat(i,:));
+        zz = Skeleton.zz(Skeleton.ConnMat(i,:));
+
+        plot3(xx, yy, zz, 'k-', 'Marker', 'o', 'MarkerSize', 10,...
+            'MarkerFaceColor', 'm', 'MarkerEdgeColor', 'm', 'LineWidth', 5);
+        hold on
+    end
+    for i = 1 : Skeleton.NodeNum
+        [x,y,z] = sphere;
+        x = x*RadVec(i);
+        y = y*RadVec(i);
+        z = z*RadVec(i);
+        surf(x+Skeleton.xx(i),y+Skeleton.yy(i),z+Skeleton.zz(i))
+        hold on
+    end
+    axis equal
+    hold off
+    fprintf("Plotting skeleton radii!\n")
+end
